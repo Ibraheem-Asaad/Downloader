7ad72e068d0df7adf61f059d3726968cb747e348
Ibrahiem <simplex3@campus.technion.ac.il>
2017-09-30 11:12:58 +0300

    Notes

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ LOGOUT_URL = ''
 
 TARGET_URL = 'http://webcourse.cs.technion.ac.il/236353/Spring2017/ho_Additional%20Materials.html'
 TARGET_FOLDER = 'C:\\Users\\brhoo_000\\Downloads'
-MAX_FILES = 100
+MAX_FILES = 3
 EXTS = {'pdf', 'doc', 'docx', 'txt'}
 REQ_CONF = False
 
@@ -25,9 +25,10 @@ def incr(max):
         i = i + 1
         yield i
 
-
+# this works because, in python
+# default parameters are static - evaluated once
 def name_mapping(name, num=incr(MAX_FILES)):
     """Changes each downloaded file name accordingly"""
     # TODO: generalize UTF-8 encoding
     # manipulate name patterns - use num.next() for incremental numbering:
-    return name.replace('%20', ' ')
+    return name.replace('%20', ' ').replace('_', ' ').replace('-', ' ')
